3edb20f66807fca2e9fb63d8811db5fc6fe84666
Dimas Abreu Dutra <dimasadutra@gmail.com>
2015-06-20 20:35:47 -0300

    Added quaternion renormalization dynamics and prior.

--- corujas.py
+++ corujas.py
@@ -1,3 +1,4 @@
+import sys
 import attrdict
 import numpy as np
 import numpy.linalg
@@ -17,27 +18,29 @@ class SymbolicModel(base.SymbolicModel):
          'q1_meas', 'q2_meas', 'q3_meas']
     '''Measurement vector.'''
     
-    p = ['x_meas_std', 'y_meas_std', 'z_meas_std',
-         'q1_meas_std', 'q2_meas_std', 'q3_meas_std']
+    p = []
     '''Parameter vector.'''
     
     s = []
     '''Exogenous signals vector.'''
     
-    c = []
+    c = ['x_meas_std', 'y_meas_std', 'z_meas_std',
+         'q1_meas_std', 'q2_meas_std', 'q3_meas_std',
+         'w0_std', 'v0_std', 'pos0_std', 'q_renorm_gain']
     '''Constants vector.'''
 
     def f(self, t, x, p, s, c):
         '''Drift function.'''
         a = attrdict.AttrDict(self.unpack_arguments(t=t, x=x, p=p, s=s, c=c))
+        renorm = a.q_renorm_gain * (1 - a.q0**2 - a.q1**2 - a.q2**2 - a.q3**2)
         return [
             a.vx, 
             a.vy, 
             a.vz, 
-            -0.5 * (a.q1 * a.wx + a.q2 * a.wy + a.q3 * a.wz),
-            -0.5 * (-a.q0 * a.wx - a.q2 * a.wz + a.q3 * a.wy),
-            -0.5 * (-a.q0 * a.wy + a.q1 * a.wz - a.q3 * a.wx),
-            -0.5 * (-a.q0 * a.wz - a.q1 * a.wy + a.q2 * a.wx),
+            -0.5 * (a.q1 * a.wx + a.q2 * a.wy + a.q3 * a.wz) + renorm * a.q0,
+            -0.5 * (-a.q0 * a.wx - a.q2 * a.wz + a.q3 * a.wy) + renorm * a.q1,
+            -0.5 * (-a.q0 * a.wy + a.q1 * a.wz - a.q3 * a.wx)  + renorm * a.q2,
+            -0.5 * (-a.q0 * a.wz - a.q1 * a.wy + a.q2 * a.wx)  + renorm * a.q3,
             0, 
             0, 
             0,
@@ -73,7 +76,18 @@ class SymbolicModel(base.SymbolicModel):
                 symstats.normal_logpdf(a.q3_meas, a.q3, a.q3_meas_std))
     
     def prior_logpdf(self, x, p, c):
-        return 0
+        a = attrdict.AttrDict(
+            self.unpack_arguments(x=x, p=p, c=c)
+        )
+        return (symstats.normal_logpdf(a.wx, 0, a.w0_std) +
+                symstats.normal_logpdf(a.wy, 0, a.w0_std) +
+                symstats.normal_logpdf(a.wz, 0, a.w0_std) +
+                symstats.normal_logpdf(a.vx, 0, a.v0_std) +
+                symstats.normal_logpdf(a.vy, 0, a.v0_std) +
+                symstats.normal_logpdf(a.vz, 0, a.v0_std) + 
+                symstats.normal_logpdf(a.x, 0, a.pos0_std) +
+                symstats.normal_logpdf(a.y, 0, a.pos0_std) +
+                symstats.normal_logpdf(a.z, 0, a.pos0_std))
 
 
 def generated_src():
@@ -107,12 +121,14 @@ def unwrap_quaternion(q):
     return unwrapped
 
 
-def load_data(filepath, range_):
+def load_data(filepath, start=None, stop=None):
+    range_ = slice(start, stop)
     data = io.loadmat(filepath)
     tmeas = data['time'].flatten()[range_]
     q = data['q'][range_]
     q_unwrapped = unwrap_quaternion(q)
     
+
     y_dict = dict(
         x_meas=data['x'].flatten()[range_],
         y_meas=data['y'].flatten()[range_],
@@ -122,6 +138,9 @@ def load_data(filepath, range_):
         q2_meas=q_unwrapped[:, 2],
         q3_meas=q_unwrapped[:, 3],
     )
+    y_dict['x_meas'] -= y_dict['x_meas'][0]
+    y_dict['y_meas'] -= y_dict['y_meas'][0]
+    y_dict['z_meas'] -= y_dict['z_meas'][0]
     return tmeas, y_dict
 
 
@@ -137,7 +156,10 @@ def spline_fit(tmeas, y_dict, smoothing_factor):
 
 
 def given_params():
-    return {}
+    return dict(q_renorm_gain=1, w0_std=1e-3, v0_std=10, pos0_std=1,
+                x_meas_std=0.04, y_meas_std=0.04, z_meas_std=0.04,
+                q0_meas_std=0.0002, q1_meas_std=0.0002,
+                q2_meas_std=0.0002, q3_meas_std=0.0002)
 
 
 def param_guess():
@@ -191,9 +213,12 @@ def save_data(tmeas, y_dict, t_fine, xopt, filename):
     io.matlab.mio.savemat(filename, data)
 
 
-def main():
-    tmeas, y_dict = load_data('datasmooth.bin', np.s_[:])
-
+def main(argv):
+    file_name = str(argv[0])
+    start = int(argv[1]) if len(argv) > 1 else None
+    stop = int(argv[2]) if len(argv) > 2 else None
+    tmeas, y_dict = load_data(file_name, start, stop)
+    
     splines = spline_fit(tmeas, y_dict, 4)
     params = {}
     params.update(given_params())
@@ -211,16 +236,20 @@ def main():
     z0 = problem.pack_decision(x_guess, None, p)
     
     p_lb = dict(x_meas_std=0, y_meas_std=0, z_meas_std=0,
-                q1_meas_std=0, q2_meas_std=0, q3_meas_std=0)
-    p_fix = dict(x_meas_std=0.2, y_meas_std=0.2, z_meas_std=0.2,
-                 q1_meas_std=0.0005, q2_meas_std=0.0005, q3_meas_std=0.0005)
+                q0_meas_std=0, q1_meas_std=0, q2_meas_std=0, q3_meas_std=0)
+    p_fix = dict()
     z_bounds = problem.pack_bounds(p_lb=p_lb, p_fix=p_fix)
     z_bounds[:, 3:7] = [y_dict['q0_meas'][0], y_dict['q1_meas'][0],
                         y_dict['q2_meas'][0],y_dict['q3_meas'][0]]
     
     nlp = problem.nlp(z_bounds)
+    nlp.num_option(b'tol', 1e-6)
+    nlp.int_option(b'max_iter', 100)
     zopt, solinfo = nlp.solve(z0)
     xopt, dopt, popt = problem.unpack_decision(zopt)
     yopt = model.meas_mean(t_fine, xopt, popt)
 
-    save_data(tmeas, y_dict, t_fine, xopt, 'filtered')
+    save_data(tmeas, y_dict, t_fine, xopt, file_name + 'meas')
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
